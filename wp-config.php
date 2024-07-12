a78d8882b37c5dd26f1d3fb04675ccae3361d3b8
YrosSO <Ibrahimasory.sow0130@gmail.com>
2024-07-12 23:55:54 +0100

    wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'sql3719440' );
+define( 'DB_NAME', '' );
 
 /** Database username */
-define( 'DB_USER', 'sql3719440' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'PKgPr4JbDu' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'sql3.freesqldatabase.com' );
+define( 'DB_HOST', '' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
